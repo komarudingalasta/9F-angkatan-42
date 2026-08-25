ecb9ac503bb242f88dc56d4d75a7a0a91c8f0bdc
komarudingalasta <komarudingalasta@gmail.com>
2026-08-25 09:14:25 +0700

    edt

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyAYEBPPm-u6o7DpKoAMeLreA3qcraJK210",
-  authDomain: "f-angkatan-42-f0c53.firebaseapp.com",
-  projectId: "f-angkatan-42-f0c53",
-  storageBucket: "f-angkatan-42-f0c53.firebasestorage.app",
-  messagingSenderId: "177506317635",
-  appId: "1:177506317635:web:9401c17af9543044a36bf6"
-};
-window.firebaseConfig = firebaseConfig;
